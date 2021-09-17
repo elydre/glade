ec8f8c25f582f627f434f2a6b1302e08a8782fde
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-09-17 19:55:10 +0200

    pf4

--- PY2C/main.py
+++ PY2C/main.py
@@ -3,15 +3,28 @@ import mod.ColorPrint as cprint
 
 class init:
     def lsprog():
-        cprint.colorprint("quel programme voullez vous convertir: ",color=cprint.Colors.blanc)
+        cprint.colorprint("\nQuel programme voullez vous convertir: ",color=cprint.Colors.blanc)
         ls_liste = cy.ls("/conteneur")
         for element in ls_liste:
             ext = element.split(".")[len(element.split("."))-1]
+            cprint.colorprint("  ",color=cprint.Colors.none,end=False)
             if ext == "py": cprint.colorprint(element,color=cprint.Colors.bleu,end=False)
             elif ext == "txt": cprint.colorprint(element,color=cprint.Colors.jaune,end=False)
-            elif ext == "cpp": cprint.colorprint(element,color=cprint.Colors.rouge,end=False)
+            elif ext == "cpp": cprint.colorprint(element,color=cprint.Colors.magenta,end=False)
             else: cprint.colorprint(element,color=cprint.Colors.blanc,end=False)
-            cprint.colorprint("  ",color=cprint.Colors.none,end=False)
-        return(input("~} "))
+        return(input("\n~} "))
 
-init.lsprog()
+    def main():
+        global todo
+        no_done = True
+        while no_done:
+            todo = init.lsprog()
+            if cy.rfil_rela("/conteneur",todo) != None: no_done = False
+            else: cprint.colorprint("ERREUR: fichier inexistant ou illisible",color=cprint.Colors.rouge)
+
+
+class converter:
+    def main():
+        cy.rfil_rela()
+
+init.main()
