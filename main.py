dfad0c449ae6abdcb81f83e471963c9953b9a5d7
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-09-20 18:16:35 +0200

    0.0.8

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+'''
+--|~|--|~|--|~|--|~|--|~|--|~|--
+
+██  ████        ██████        ██
+████    ██     ██           ████
+██      ██   ████████     ██  ██
+████████       ██       ██    ██
+██             ██       █████████
+██             ██             ██
+██
+.codé en : UTF-8
+.langage : python 3
+.v       : 0.0.8
+--|~|--|~|--|~|--|~|--|~|--|~|--
+'''
 import mod.cytron as cy
 import mod.ColorPrint as cprint
 
 class sys:
     def info(msg):
         cprint.colorprint("|end| ",color=cprint.Colors.cyan,end=False)
-        cprint(msg,color=cprint.Colors.blanc)
+        cprint.colorprint(msg,color=cprint.Colors.blanc)
 
     def gen_err(msg):
         cprint.colorprint("|err| ",color=cprint.Colors.rouge,end=False)
-        cprint(msg,color=cprint.Colors.blanc)
+        cprint.colorprint(msg,color=cprint.Colors.blanc)
 
 class init:
     def lsprog():
         cprint.colorprint("\nWhich program to convert: ",color=cprint.Colors.blanc)
-        ls_liste = cy.ls("/contener")
+        ls_liste = cy.ls("/container")
         for element in ls_liste:
             ext = element.split(".")[len(element.split("."))-1]
             cprint.colorprint(" ",color=cprint.Colors.none,end=False)
@@ -29,33 +44,32 @@ class init:
         no_done = True
         while no_done:
             todo = init.lsprog()
-            if cy.rfil_rela("/contener",todo) != None: no_done = False
+            if cy.rfil_rela("/container",todo) != None: no_done = False
             else: sys.gen_err("non-existent or unreadable file")
 
 class maker:
     def main():
-        name = str(todo.split(".")[len(todo.split("."))-1]) + ".cpp"
-        cy.mkfil("/conteneur",name,"".join((l+"\n") for l in EXIT))
+        name = str(todo.split(".")[len(todo.split("."))-2]) + ".cpp"
+        cy.mkfil("/container",name,"".join((l+"\n") for l in EXIT))
 
 class converter:
     def edit_l(l):
 
         l = str(l)
         if l.startswith("if "):
-            cont = l.split("if ")[1] #on enleve le 'if '
-            cont = cont.split(":")[len(cont.split(":"))-2] #on enleve le ':'
-
-            print(cont)
-            EXIT.append("if(" + cont + ")")
+            cont = l.split("if ")[1] #we remove the 'if '
+            cont = cont.split(":")[len(cont.split(":"))-2] #we remove the ':'
+            EXIT.append("if (" + cont + ")")
             EXIT.append("{")
 
 
     def main():
-        fichier = cy.rfil_rela("/contener",todo)
+        fichier = cy.rfil_rela("/container",todo)
         ligues = fichier.split("\n")
 
-        global EXIT
-        EXIT = []
+        global EXIT, COMP
+        EXIT = [] # list of 'compiled' code
+        COMP = [] # list of structure
 
         for l in ligues:
             converter.edit_l(l)
