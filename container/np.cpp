4fb9eab9a5ad59e13e058a9ca2f05acbc747ec44
pf4 <74263128+pf4-DEV@users.noreply.github.com>
2021-09-23 12:35:50 +0200

    pf4

--- container/np.cpp
+++ container/np.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 int main()
 {
-cout << "runing" << endl;
-    long long int max = 0;
-    for (long int i = 0; i <= 4000; i = i + 1)
-    {
-        max = pow(i,0.5) + 1;
-        if (i%2 != 0)
+    cout << "runing" << endl;
+        long long int max = 0;
+        for (long int i = 0; i <= 4000; i = i + 1)
         {
-            for (long int x = 2; x <=  max; x = x + 1)
+            max = pow(i,0.5) + 1;
+            if (i%2 != 0)
             {
-                if (i%x == 0)
+                for (long int x = 2; x <=  max; x = x + 1)
                 {
-                    break;
-                }
-                if (x == max)
-                {
-                    cout << i << endl;
+                    if (i%x == 0)
+                    {
+                        break;
+                    }
+                    if (x == max)
+                    {
+                        cout << i << endl;
 }
 }
 }
--- container/np.py
+++ container/np.py
@@ -3,13 +3,13 @@
 using namespace std
 
 def main():
-print("runing")
-    long long int max = 0
-    for i in range(4000):
-        max = pow(i,0.5) + 1
-        if i%2 != 0:
-            for x in range(2, max):
-                if i%x == 0:
-                    break
-                if x == max:
-                    print(i)
+    print("runing")
+        long long int max = 0
+        for i in range(4000):
+            max = pow(i,0.5) + 1
+            if i%2 != 0:
+                for x in range(2, max):
+                    if i%x == 0:
+                        break
+                    if x == max:
+                        print(i)
