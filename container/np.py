81f414aa20533748691cd7ad16fb391fd118613b
pf4 <74263128+pf4-DEV@users.noreply.github.com>
2021-09-23 21:01:45 +0200

    pf4

--- container/np.py
+++ container/np.py
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <cmath>
-using namespace std
-
 def main():
     print("runing")
         long long int max = 0
--- container/t.cpp
+++
@@ -1,8 +0,0 @@
-// interpreted and compiled by GLADE
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "test" << endl;
-    return 0;
-}
