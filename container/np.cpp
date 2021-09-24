1dc9061916157f6fbe5aecebf010d781ba6ae1c5
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-09-24 09:44:43 +0200

    np

--- container/np.cpp
+++ container/np.cpp
@@ -5,23 +5,23 @@ using namespace std;
 int main()
 {
     cout << "runing" << endl;
-        long long int max = 0;
-        for (long int i = 0; i <= 4000; i = i + 1)
+    long long int max = 0;
+    for (long int i = 0; i <= 4000; i = i + 1)
+    {
+        max = pow(i,0.5) + 1;
+        if (i%2 != 0)
         {
-            max = pow(i,0.5) + 1;
-            if (i%2 != 0)
+            for (long int x = 2; x <=  max; x = x + 1)
             {
-                for (long int x = 2; x <=  max; x = x + 1)
+                if (i%x == 0)
                 {
-                    if (i%x == 0)
-                    {
-                        break;
-                    }
-                    if (x == max)
-                    {
-                        cout << i << endl;
-}
-}
-}
-}
+                    break;
+                }
+                if (x == max)
+                {
+                    cout << i << endl;
+                }
+            }
+        }
+    }
 }
--- container/np.py
+++ container/np.py
@@ -2,12 +2,12 @@
 
 def main():
     print("runing")
-        long long int max = 0
-        for i in range(4000):
-            max = pow(i,0.5) + 1
-            if i%2 != 0:
-                for x in range(2, max):
-                    if i%x == 0:
-                        break
-                    if x == max:
-                        print(i)
+    long long int max = 0
+    for i in range(4000):
+        max = pow(i,0.5) + 1
+        if i%2 != 0:
+            for x in range(2, max):
+                if i%x == 0:
+                    break
+                if x == max:
+                    print(i)
