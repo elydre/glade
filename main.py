87af411f0876932b428f548d3612c7d7817ad7b5
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-09-30 20:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
  - v       : 0.1.8
 --|~|--|~|--|~|--|~|--|~|--|~|--
 '''
+from posixpath import split
 import mod.cytron as cy
 import mod.ColorPrint as cprint
 
@@ -56,12 +57,13 @@ class init:
             else: sys.gen_err("non-existent or unreadable file")
 
 class teyes:
-
+    
     def var_type(cont):
         return(type(cont))
 
 
     def edit_l(l,nb,len_tot):
+        global ATOC
         def tab_c(l):
             t = 0
             nb_tab = 4 #nombre d'espace dans une TAB
@@ -81,6 +83,10 @@ class teyes:
             cont , to_del = str(cont) , str(to_del)
             return(cont.split(to_del)[len(cont.split(to_del))-2])
 
+        def contient(var,cont):
+            cont, var = str(cont), str(var)
+            return(len(var.split(cont))-1)
+
         l = str(l)
 
         TAB.append(tab_c(l))
@@ -88,40 +94,52 @@ class teyes:
         l = del_tab(l)
 
         if l != "" or nb == len_tot-1:
-            for loop in range(1,TAB[nb-1]-TAB[nb]+1): EYES.append([TAB[nb-1]-1*loop,"}"])
+            for loop in range(1,TAB[nb-1]-TAB[nb]+1):
+                temp = ""
+                for a in range(len(ATOC.split("/"))-1):
+                    if a != 0: temp += "/" + ATOC.split("/")[a]
+                ATOC = temp
+                EYES.append([ATOC,TAB[nb-1]-1*loop,"}"])
+                
+                
         
         if nb == 0:
-            EYES.append([TAB[nb],"comm","interpreted and compiled by GLADE"])
+            EYES.append([ATOC,TAB[nb],"comm","interpreted and compiled by GLADE"])
 
         if l.startswith("if "):
             cont = l.split("if ")[1]
             cont = del_end(cont,":")
-            EYES.append([TAB[nb],"if",cont])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"if",cont])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/if"
 
         elif l.startswith("elif "):
             cont = l.split("elif ")[1]
             cont = del_end(cont,":")
-            EYES.append([TAB[nb],"elif",cont])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"elif",cont])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/elif"
 
         elif l.startswith("else "):
             cont = l.split("else ")[1]
             cont = del_end(cont,":")
-            EYES.append([TAB[nb],"else",cont])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"else",cont])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/else"
 
         elif l.startswith("while "):
             cont = l.split("while ")[1]
             cont = del_end(cont,":")
-            EYES.append([TAB[nb],"while",cont])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"while",cont])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/while"
 
         elif l.startswith("def "):
             cont = l.split("def ")[1]
             cont = del_end(cont,":")
-            EYES.append([TAB[nb],"def",cont])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"def",cont])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/" + cont.split("(")[0]
 
         elif l.startswith("for "):
             cont = l.split("for ")[1]
@@ -137,66 +155,75 @@ class teyes:
             elif len(arg) == 3:
                 pas = arg[2]
 
-            EYES.append([TAB[nb],"for",[var_name,min,max,pas]])
-            EYES.append([TAB[nb],"{"])
+            EYES.append([ATOC,TAB[nb],"for",[var_name,min,max,pas]])
+            EYES.append([ATOC,TAB[nb],"{"])
+            ATOC += "/for"
 
         elif l.startswith("print("):
             cont = l.split("print(")[1]
             cont = del_end(cont,")")
-            EYES.append([TAB[nb],"print",cont])
+            EYES.append([ATOC,TAB[nb],"print",cont])
 
         elif l.startswith("return("):
             cont = l.split("return(")[1]
             cont = del_end(cont,")")
-            EYES.append([TAB[nb],"return",cont])
+            EYES.append([ATOC,TAB[nb],"return",cont])
 
         elif l.startswith("#"):
             lb = del_tab(l.split("#")[1])
             if lb.startswith("include "):
                 cont = lb.split("include ")[1]
-                EYES.append([TAB[nb],"include",cont])
+                EYES.append([ATOC,TAB[nb],"include",cont])
             elif lb.startswith("!"):
                 cont = del_tab(lb.split("!")[1])
-                EYES.append([TAB[nb],"lnb",cont])
+                EYES.append([ATOC,TAB[nb],"lnb",cont])
             else:
-                EYES.append([TAB[nb],"comm",lb])
+                EYES.append([ATOC,TAB[nb],"comm",lb])
+
+        elif contient(l,"=") == 1:
+            nom = l.split("=")[0]
+            cont = del_tab(l.split("=")[1])
+            EYES.append([ATOC,TAB[nb],"vare",[nom,cont]])
+            VAR.append([nom,cont])
 
         elif del_tab(l) != "":
-            EYES.append([TAB[nb],"unknown",l])
+            EYES.append([ATOC,TAB[nb],"unknown",l])
 
     def main():
         pprint = False
         fichier = cy.rfil_rela("/container",todo)
         ligues = fichier.split("\n")
         ligues.append("")
-        global EYES, TAB
+        global EYES, TAB, VAR, ATOC
         EYES = [] # liste de code token eyes
+        VAR = []  # liste des variables
         TAB = []  # liste des TAB
+        ATOC = ""
 
         for nb in range(len(ligues)):
             l = ligues[nb]
             teyes.edit_l(l,nb,len(ligues))
-        
+
         # relecture
         for e in EYES:
-            if e[1] == "print" and pprint == False:
-                sys.dev("importation de print automatique")
-                EYES.insert(1,[0, "include", "<iostream>"])
-                EYES.insert(2,[0, 'unknown', 'using namespace std'])
+            if e[2] == "print" and pprint == False:
+                sys.app("importation de print automatique")
+                EYES.insert(1,['',0, "include", "<iostream>"])
+                EYES.insert(2,['',0, 'unknown', 'using namespace std'])
                 pprint = True
-
     
         # print (dev)
+        sys.app("liste des variables: " + str(VAR))
         for e in EYES:
-            sys.app(str(e))
+            sys.dev(str(e))
 
 class compiler:
     def edit_e(e):
         def add_tab(tab): return(" "*tab*4)
         
-        de = e[1]  #element detecte
-        tab = e[0] #nb de tab
-        try: arg = e[2] #arg
+        de = e[2]  #element detecte
+        tab = e[1] #nb de tab
+        try: arg = e[3] #arg
         except: arg = None
 
         if de == "def":
@@ -233,6 +260,9 @@ class compiler:
 
         elif de == "comm":
             EXIT.append(add_tab(tab) + "// " + arg)
+
+        elif de == "vare":
+            EXIT.append(add_tab(tab) + arg[0] + "= " + arg[1] + ";")
 
         elif de == "unknown":
             EXIT.append(add_tab(tab) + arg + ";")
