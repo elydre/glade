9b5ab547b5067d2fe799420845549329b8c03ca1
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-10-17 09:57:31 +0200

    ide dt update

--- direct time.py
+++ direct time.py
@@ -9,19 +9,21 @@ fenetre.configure(background="#000000")
 
 cont = ""
 
-py = tk.Text(fenetre, width=30,background="#212338",foreground="#b1e1f0",insertbackground="#00ffff")
+py = tk.Text(fenetre, width=30,background="#212338",foreground="#b1e1f0",insertbackground="#00ffff",font=("consolas", 12))
 py.place(x= 10 ,y=10,width= 460,height=680)
 
-cpp = tk.Text(fenetre, width=30, background="#212338",foreground="#b1e1f0")
+cpp = tk.Text(fenetre, width=30, background="#212338",foreground="#b1e1f0",font=("consolas", 12))
 cpp.place(x= 480 ,y=10,width= 460,height=680)
 cpp.configure(state='disabled')
 
 def py_actu():
     global cont
-    if py.get("0.0","end") != cont:
-        cont = py.get("0.0","end")
+    temp = py.get("0.0","end")
+    while "	" in temp: temp = temp.replace("	","    ")
+    if temp != cont:
+        cont = temp
         cy.mkfil("/container","DirectTime.py",cont)
-    py.after(500,py_actu)
+    py.after(250,py_actu)
 
 def cpp_actu():
     temp = cy.rfil_rela("/container","DirectTime.cpp")
@@ -31,7 +33,7 @@ def cpp_actu():
         cpp.delete ("0.0", "end")
         cpp.insert(0.0, (temp if temp != "None" else "merci de lancer glade"))
         cpp.configure(state='disabled')
-    cpp.after(500,cpp_actu)
+    cpp.after(250,cpp_actu)
 
 cpp_actu()
 py_actu()
