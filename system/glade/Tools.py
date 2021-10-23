771f997b3f429fd2405f6f2d0ae8a9a8cdf670b4
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-10-23 16:39:33 +0200

    Update Tools.py

--- system/glade/Tools.py
+++ system/glade/Tools.py
@@ -9,10 +9,10 @@ version = "0.10"
 # init
 
 class init:
-    def __init__(self, edit=True, todo = None, debug_print = True, sys_print = True, auto_main = True, init_var = True, auto_include = True, make_log = True, loop_compil = False, space_in_tabs = 4, int_var_type = "long int"):
+    def __init__(self, edit=True, todo = None, debug_print = True, sys_print = True, auto_main = True, init_var = True, auto_include = True, make_log = True, loop_compil = False, space_in_tabs = 4, int_var_type = "long int"): # sourcery no-metrics
         para_edit = 0
         #lecture du fichier de paramètres
-        if cy.rfil_rela("/system","settings.txt") == None: war("fichier de paramètres non trouvé")
+        if cy.rfil_rela("/system", "settings.txt") is None: war("fichier de paramètres non trouvé")
         elif edit:
             for p in cy.rfil_rela("/system","settings.txt").split("\n"):
                 if not(p.startswith("#")) and len(p.split("=")) > 1:
@@ -21,57 +21,56 @@ class init:
                     atr = p.split("=")[1].strip()
 
                     if var == "todo":
-                        if atr == "None": todo = None
-                        else: todo = atr
+                        todo = None if atr == "None" else atr
 
                     elif var == "int var type":
                         int_var_type = atr
 
                     elif var == "debug print":
-                        if atr == "False" or atr == "false": debug_print = False
-                        elif atr == "True" or atr == "true": debug_print = True
+                        if atr in ["False", "false"]: debug_print = False
+                        elif atr in ["True", "true"]: debug_print = True
                         else: war("valleur non bool pour debug print (False par defaut)\n      ici -> " + str(atr))
 
                     elif var == "sys print":
-                        if atr == "False" or atr == "false": sys_print = False
-                        elif atr == "True" or atr == "true": sys_print = True
+                        if atr in ["False", "false"]: sys_print = False
+                        elif atr in ["True", "true"]: sys_print = True
                         else: war("valleur non bool pour debug print (True par defaut)\n      ici -> " + str(atr))
 
                     elif var == "make log":
-                        if atr == "False" or atr == "false": make_log = False
-                        elif atr == "True" or atr == "true": make_log = True
+                        if atr in ["False", "false"]: make_log = False
+                        elif atr in ["True", "true"]: make_log = True
                         else: war("valleur non bool pour make log (True par defaut)\n      ici -> " + str(atr))
 
                     elif var == "init var":
-                        if atr == "False" or atr == "false": init_var = False
-                        elif atr == "True" or atr == "true": init_var = True
+                        if atr in ["False", "false"]: init_var = False
+                        elif atr in ["True", "true"]: init_var = True
                         else: war("valleur non bool pour init var (True par defaut)\n      ici -> " + str(atr))
 
                     elif var == "auto main":
-                        if atr == "False" or atr == "false": auto_main = False
-                        elif atr == "True" or atr == "true": auto_main = True
+                        if atr in ["False", "false"]: auto_main = False
+                        elif atr in ["True", "true"]: auto_main = True
                         else: war("valleur non bool pour auto main (True par defaut)\n      ici -> " + str(atr))
 
                     elif var == "loop compil":
-                        if atr == "False" or atr == "false": loop_compil = False
-                        elif atr == "True" or atr == "true": loop_compil = True
+                        if atr in ["False", "false"]: loop_compil = False
+                        elif atr in ["True", "true"]: loop_compil = True
                         else: war("valleur non bool pour loop compil (False par defaut)\n      ici -> " + str(atr))
 
                     elif var == "auto include":
-                        if atr == "False" or atr == "false": auto_include = False
-                        elif atr == "True" or atr == "true": auto_include = True
+                        if atr in ["False", "false"]: auto_include = False
+                        elif atr in ["True", "true"]: auto_include = True
                         else: war("valleur non bool pour auto include (True par defaut)\n      ici -> " + str(atr))
-                    
+
                     elif var == "space in tabs":
                         try: space_in_tabs = int(atr)
                         except: war("valleur non int pour space in tabs (4 par defaut)\n      ici -> " + str(atr))
-                    
+
                     else:
                         para_edit -= 1
                         gen_err("paramètres inconnu\n      ici -> " + str(p))
 
             info(str(para_edit)+" paramètres édités")
-            
+
         self.todo = todo
         self.debug_print = debug_print
         self.space_in_tabs = space_in_tabs
@@ -97,7 +96,7 @@ def request(settings):
             elif ext == "cpp": cprint.colorprint(element,color=cprint.Colors.magenta,ligne=True,end=False)
             else: cprint.colorprint(element,color=cprint.Colors.blanc,end=False)
         print("\n")
-        if settings.todo == None or settings.todo == "":
+        if settings.todo is None or settings.todo == "":
             inp = input("~} ")
         else:
             cprint.colorprint("(",color=cprint.Colors.blanc, end=False)
@@ -108,23 +107,22 @@ def request(settings):
 
         if not(inp.startswith("!")):
             settings.todo = inp
-            if cy.rfil_rela("/container",settings.todo) != None: no_done = False
-            else: gen_err("fichier non existent ou illisible")
-        else:
-            if inp == "!r":
-                settings = init()
-                info("paramètres rechargé")
+            if cy.rfil_rela("/container", settings.todo) is None: gen_err("fichier non existent ou illisible")
+            else: no_done = False
+        elif inp == "!r":
+            settings = init()
+            info("paramètres rechargé")
 
-            elif inp == "!c":
-                cy.clear()
+        elif inp == "!c":
+            cy.clear()
 
-            elif inp == "!v":
-                print(f"version de Cytron:            {cy.version()}")
-                print(f"version de la boite à outils: {version}")
-                print(f"version du TokenEyes:         {TEversion}")
-                print(f"version du compilateur:       {GCversion}")
+        elif inp == "!v":
+            print(f"version de Cytron:            {cy.version()}")
+            print(f"version de la boite à outils: {version}")
+            print(f"version du TokenEyes:         {TEversion}")
+            print(f"version du compilateur:       {GCversion}")
 
-            else: gen_err("commande existente")
+        else: gen_err("commande existente")
     return(settings)
 
 # print
@@ -174,7 +172,7 @@ def printlog(MSG):
             dev(e[1])
         elif e[0] == "gen_err":
             gen_err(f"l.{e[2]}: {e[1]}")
-        elif e[0] == "war" or e[0] == "c_war":
+        elif e[0] in ["war", "c_war"]:
             war(f"l.{e[2]}: {e[1]}")
         else:
             war(f"printlog inconnu -> {e}")
@@ -190,7 +188,7 @@ def del_end(cont,to_del):
     return(cont[0:-len(to_del)]if to_del in cont else cont)
 
 def iic(liste, e, p):
-    return(True if e in [v[p] for v in liste] else False)
+    return e in [v[p] for v in liste]
 
 def tab_c(space_in_tabs,l):
     t = 0
@@ -205,14 +203,14 @@ def varitype(var,cont,settings,allvar):
     for v in allvar:
         if cont == v[1]:
             typ = v[4]
-    if typ == None and cont.startswith("#"):
-        if cont == "#int": typ = settings.int_var_type
-        elif cont == "#bool": typ = "bool"
-        elif cont == "#float": typ = "float"
-        else: typ = "string"
-    elif typ == None:
-        if len(cont.split('"')) > 1 or len(cont.split("'")) > 1: typ = "string"
-        elif cont == "true" or cont == "false": typ = "bool"
+    if typ is None:
+        if cont.startswith("#"):
+            if cont == "#int": typ = settings.int_var_type
+            elif cont == "#bool": typ = "bool"
+            elif cont == "#float": typ = "float"
+            else: typ = "string"
+        elif len(cont.split('"')) > 1 or len(cont.split("'")) > 1: typ = "string"
+        elif cont in ["true", "false"]: typ = "bool"
         else:
             try: int(cont) ; typ = settings.int_var_type
             except:
@@ -220,6 +218,6 @@ def varitype(var,cont,settings,allvar):
                 except:
                     typ = settings.int_var_type
                     m[0] = ["c_war", f"type inconnu ici -> {cont}"]
-    
+
     if settings.debug_print: m[1] = ["dev", f"création de variable automatique: '{var}' de type '{typ}'"]
     return(m,[var,typ])
